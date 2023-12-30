d4eaa0c2cc2e81e076a2c1f97cc516071c169583
Chilling Fingers <eshan.bharadwaj04@gmail.com>
2023-12-30 02:45:06 -0600

    updated newsArticles array

--- components/news-section.tsx
+++ components/news-section.tsx
@@ -10,29 +10,6 @@ import { useState, useEffect } from "react";
 import NewsCard from "@/components/news-card";
 import { Pagination, Progress } from "@nextui-org/react";
 
-// const newsArticles = [
-//   {
-//     title: "Auto Workers Had a Great Year. But Can It Continue?",
-//     description:
-//       "After decades of decline, the UAW scored big victories in 2023. Here's what to watch for in 2024",
-//     author: "Kristin Dziczek",
-//     publishedAt: "2023-12-15T19:26:35Z",
-//     url: "https://www.wsj.com/business/autos/united-auto-workers-2023-e12c1400",
-//     urlToImage: "https://images.wsj.net/im-900482/social",
-//   },
-//   {
-//     title:
-//       "Commodities Carriers Star Bulk and Eagle Bulk Shipping Agree to $2.1 Billion Merger",
-//     description:
-//       "The marriage of the New York-listed ocean carriers aims to boost investor interest",
-//     author: "Costas Paris",
-//     publishedAt: "2023-12-12T01:46:00Z",
-//     url: "https://www.wsj.com/business/energy-oil/commodities-carriers-star-bulk-and-eagle-bulk-shipping-agree-to-2-1-billion-merger-3a140a07",
-//     urlToImage: "https://images.wsj.net/im-899070/social",
-//   },
-//   // Add more articles as needed
-// ];
-
 const NewsSection = () => {
   const [newsArticles, setNewsArticles] = useState<NewsArticle[]>([]);
   const [currentPage, setCurrentPage] = useState(1); // Track the current page
